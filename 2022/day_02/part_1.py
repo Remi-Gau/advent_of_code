d85c7bc8d432111821fc3aea9281cf3f292e7fa9
Remi Gau <remi_gau@hotmail.com>
2022-12-03 13:09:46 +0100

    complete day 2 part 2

--- 2022/day_02/part_1.py
+++ 2022/day_02/part_1.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 from rich import print
 
+from utils import load_input
+
 """
 A for Rock, B for Paper, and C for Scissors
 X for Rock, Y for Paper, and Z for Scissors
 1 for Rock, 2 for Paper, and 3 for Scissors
 """
+
+draw_conditions = ["C Z", "A X", "B Y"]
+win_conditions = ["B Z", "C X", "A Y"]
 
 win_value = 6
 draw_value = 3
@@ -15,43 +20,48 @@ rock_value = 1
 paper_value = 2
 scissors_value = 3
 
-draw_conditions = ["C Z", "A X", "B Y"]
-win_conditions = ["B Z", "C X", "A Y"]
-
 verbose = False
 
-input_file = Path(__file__).parent.joinpath("input.txt")
+input_data = load_input()
 
-with input_file.open() as f:
-    input_data = f.read()
-
-score = []
-for row in input_data.splitlines():
-
-    print(row)
-
+def return_shape_score(row):
     shape_score = 0
-    outcome_score = 0
-
     if row[2] == "X":
         shape_score = rock_value
     elif row[2] == "Y":
         shape_score = paper_value
     elif row[2] == "Z":
         shape_score = scissors_value
-    print(f"shape score is {shape_score}")
+    return shape_score
 
+def return_outcome_score(row):
+    outcome_score = 0
     if row in draw_conditions:
         outcome_score = draw_value
     elif row in win_conditions:
         outcome_score = win_value
-    print(f"outcome score is {outcome_score}")
+    return outcome_score
+
+
+score = []
+for row in input_data.splitlines():
+
+    shape_score = return_shape_score(row)
+
+    outcome_score = return_outcome_score(row)
 
     score_round = shape_score + outcome_score
-    print(f"round score is {score_round}")
 
     score.append(score_round)
+
+    if verbose:
+        print(row)
+        print(f"shape score is {shape_score}")
+        print(f"outcome score is {outcome_score}")
+        print(f"round score is {score_round}")
 
 total_score = sum(score)
 
 print(f"total score is {total_score}")
+
+assert total_score == 11841
---
+++ 2022/day_02/part_2.py
@@ -0,0 +1,86 @@
+from pathlib import Path
+from rich import print
+
+from utils import load_input
+
+"""
+A for Rock, B for Paper, and C for Scissors
+
+X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+
+1 for Rock, 2 for Paper, and 3 for Scissors
+"""
+
+win_value = 6
+draw_value = 3
+lose_value = 0
+
+rock_value = 1
+paper_value = 2
+scissors_value = 3
+
+verbose = False
+
+input_data = load_input()
+
+def return_shape_score(shape):
+    shape_score = 0
+    if shape == "A":
+        shape_score = rock_value
+    elif shape == "B":
+        shape_score = paper_value
+    elif shape == "C":
+        shape_score = scissors_value
+    return shape_score
+
+def return_outcome_score(row):
+    if row[2] == "X":
+        outcome_score = 0
+    elif row[2] == "Y":
+        outcome_score = 3
+    elif row[2] == "Z":
+        outcome_score = 6
+    return outcome_score
+
+
+score = []
+for row in input_data.splitlines():
+
+    outcome_score = return_outcome_score(row)
+
+    if outcome_score == 0:
+        if row[0] == "A":
+            shape = "C"
+        elif row[0] == "B":
+            shape = "A"
+        elif row[0] == "C":
+            shape = "B"
+
+    elif outcome_score == 3:
+        shape = row[0]
+
+    elif outcome_score == 6:
+        if row[0] == "A":
+            shape = "B"
+        elif row[0] == "B":
+            shape = "C"
+        elif row[0] == "C":
+            shape = "A"
+
+    shape_score = return_shape_score(shape)
+
+    score_round = shape_score + outcome_score
+
+    score.append(score_round)
+
+    if verbose:
+        print(row)
+        print(f"shape score is {shape_score}")
+        print(f"outcome score is {outcome_score}")
+        print(f"round score is {score_round}")
+
+total_score = sum(score)
+
+print(f"total score is {total_score}")
+
+# assert total_score == 11841
