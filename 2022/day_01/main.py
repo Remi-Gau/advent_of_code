14965715aef10a242f8b6df76cb8589350970984
Remi Gau <remi_gau@hotmail.com>
2022-12-03 12:17:52 +0100

    do day 1

---
+++ 2022/day_01/main.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from rich import print
+
+verbose = False
+
+input_file = Path(__file__).parent.joinpath("input.txt")
+
+with input_file.open() as f:
+    input_data = f.read()
+
+elves = []
+this_elf = []
+for row in input_data.splitlines():
+
+    if row == "":
+        elves.append(sum(this_elf))
+        if verbose:
+            print(this_elf)
+            print(sum(this_elf))
+        this_elf = []
+
+    else:
+        this_elf.append(int(row))
+
+if verbose:
+    print(elves)
+
+print(f"max value is {max(elves)}")
