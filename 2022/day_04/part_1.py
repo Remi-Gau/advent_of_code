78896512592a21bcc17ab80638736bf55aae4740
Remi Gau <remi_gau@hotmail.com>
2022-12-04 10:06:00 +0100

    day 4 part 1

---
+++ 2022/day_04/part_1.py
@@ -0,0 +1,38 @@
+from rich import print
+
+from utils import load_input
+
+verbose = False
+
+input_data = load_input()
+
+full_overlap_count = 0
+
+for line in input_data:
+
+    if line == "":
+        continue
+
+    elves = line.split(",")
+
+    if verbose:
+        print()
+        print(line)
+        print(elves)
+
+    id_ranges = []
+
+    for elf in elves:
+
+        this_range = set(range(int(elf.split("-")[0]), int(elf.split("-")[1]) + 1))
+        id_ranges.append(this_range)
+
+        if verbose:
+            print(elf.split("-"))
+            print(id_ranges)
+
+    if id_ranges[0].issubset(id_ranges[1]) or id_ranges[1].issubset(id_ranges[0]):
+        full_overlap_count += 1
+
+
+print(f"Full overlap count:{full_overlap_count}")
---
+++ 2022/day_04/utils.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+def load_input():
+
+    input_file = Path(__file__).parent.joinpath("input.txt")
+
+    with input_file.open() as f:
+        return  f.read().split('\n')
