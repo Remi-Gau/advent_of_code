b79b2278526e79236a01a79f7d515c00374ae428
Remi Gau <remi_gau@hotmail.com>
2022-12-11 18:11:27 +0100

    day 8 part 1

--- 2022/day_08/main.py
+++ 2022/day_08/main.py
@@ -25,39 +25,76 @@ def turn_into_df(input_data):
 def set_all_cols_to_true(grid, i_row, cols):
     for i_col in cols:
         grid.loc[i_row, i_col] = True
+    return grid
 
 
-def check_visible_in_row(grid, visibility_grid, i_row_max, i_row, cols: list):
+def check_visible_in_row(grid, visibility_grid, this_max, i_row, cols: list):
 
     current_min = 0
 
     for i_col in cols:
 
         # once we reach the max, we stop: the rest of the row won't be visible
-        if int(grid.loc[i_row, i_col]) == int(i_row_max):
+        if grid.loc[i_row, i_col] == this_max:
             visibility_grid.loc[i_row, i_col] = True
             return visibility_grid
 
         # trees on the edge are always visible
         if i_col in [cols[0]]:
-            current_min = int(grid.loc[i_row, i_col])
+            current_min = grid.loc[i_row, i_col]
             visibility_grid.loc[i_row, i_col] = True
 
-        elif int(grid.loc[i_row, i_col]) > current_min:
-            current_min = int(grid.loc[i_row, i_col])
+        elif grid.loc[i_row, i_col] > current_min:
+            current_min = grid.loc[i_row, i_col]
             visibility_grid.loc[i_row, i_col] = True
+
+        elif visibility_grid.loc[i_row, i_col] is True:
+            continue
+
+        else:
+            visibility_grid.loc[i_row, i_col] = False
+
+    return visibility_grid
+
+
+def check_visible_in_col(grid, visibility_grid, this_max, i_col, rows: list):
+
+    current_min = 0
+
+    for i_row in rows:
+
+        # once we reach the max, we stop: the rest of the row won't be visible
+        if grid.loc[i_row, i_col] == this_max:
+            visibility_grid.loc[i_row, i_col] = True
+            return visibility_grid
+
+        # trees on the edge are always visible
+        if i_col in [rows[0]]:
+            current_min = grid.loc[i_row, i_col]
+            visibility_grid.loc[i_row, i_col] = True
+
+        elif int(grid.loc[i_row, i_col]) > int(current_min):
+            current_min = grid.loc[i_row, i_col]
+            visibility_grid.loc[i_row, i_col] = True
+
+        elif visibility_grid.loc[i_row, i_col] is True:
+            continue
+
+        else:
+            visibility_grid.loc[i_row, i_col] = False
 
     return visibility_grid
 
 
 def main():
 
-    input_data = load_input(True)
+    input_data = load_input(test=False)
 
     grid = turn_into_df(input_data)
     visibility_grid = grid.copy()
 
     row_max = grid.max(axis=1)
+    col_max = grid.max(axis=0)
 
     cols = list(grid.columns)
     rows = grid.index
@@ -65,7 +102,7 @@ def main():
     for i_row in rows:
 
         if i_row in [rows[0], rows[-1]]:
-            set_all_cols_to_true(visibility_grid, i_row, cols)
+            visibility_grid = set_all_cols_to_true(visibility_grid, i_row, cols)
             continue
 
         visibility_grid = check_visible_in_row(
@@ -74,6 +111,18 @@ def main():
         visibility_grid = check_visible_in_row(
             grid, visibility_grid, row_max[i_row], i_row, list(reversed(cols))
         )
+
+    for i_col in cols:
+
+        visibility_grid = check_visible_in_col(
+            grid, visibility_grid, col_max[i_col], i_col, rows
+        )
+
+        visibility_grid = check_visible_in_col(
+            grid, visibility_grid, col_max[i_col], i_col, list(reversed(rows))
+        )
+
+    # visibility_grid.replace(list(range(10)), value=False, inplace=True)
 
     print("\ngrid")
     print(grid)
