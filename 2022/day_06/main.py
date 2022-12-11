f539312a428d9b946a56b27bc350ee623662f88c
Remi Gau <remi_gau@hotmail.com>
2022-12-11 11:30:00 +0100

    day 6 part 2

--- 2022/day_06/main.py
+++ 2022/day_06/main.py
@@ -2,19 +2,21 @@ from rich import print
 from utils import load_input
 
 
+marker_length = 14
+
 input_data = load_input()
 
 buffer = []
 
 for i, data in enumerate(input_data[0]):
 
-    if len(buffer) < 4:
+    if len(buffer) < marker_length:
         buffer.append(data)
         print(buffer)
 
-    if len(buffer) == 4:
+    if len(buffer) == marker_length:
 
-        if len(set(buffer)) == 4:
+        if len(set(buffer)) == marker_length:
             print(i)
             break
 
